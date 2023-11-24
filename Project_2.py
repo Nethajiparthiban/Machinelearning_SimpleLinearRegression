1027e6f520666bb297b6ec6e6d0a3d025fd0eff8
Nethajiparthinan <Nethajiparthiban@gmail.com>
2023-11-24 11:20:40 +0530

    2nd comment project done till prediction

--- Project_2.py
+++ Project_2.py
@@ -1,1 +1,13 @@
+#importing the modules
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#Reading the Data set
+data=pd.read_csv("D:\Git\Git-Projects\ML--Simple-Linear-Regression\salary_data.csv")
+#checking the imported data
+#print(data.head(3))
+#checking shape of  the data.
+#print(data.shape)
+#checking the Info of the data.
+print(data.info())
 
